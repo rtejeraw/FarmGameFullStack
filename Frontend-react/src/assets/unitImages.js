0854e88a258f60e6cab9306dfebfea98b89a5207
Raul Tejera <rtejera.w@gmail.com>
2026-03-14 01:09:04 -0500

    img src fix

---
+++ Frontend-react/src/assets/unitImages.js
@@ -0,0 +1,22 @@
+import cabbage from "./cabbage.png";
+import cabbageseed from "./cabbageseed.png";
+import carrot from "./carrot.png";
+import carrotseed from "./carrotseed.png";
+import cow from "./cow.png";
+import cowbreed from "./cowbreed.png";
+import sheep from "./sheep.png";
+import sheepbreed from "./sheepbreed.png";
+
+// Mapeo por nombre normalizado (el mismo que usas en item.name)
+const unitImages = {
+	cabbage: cabbage,
+	cabbageseed: cabbageseed,
+	carrot: carrot,
+	carrotseed: carrotseed,
+	cow: cow,
+	cowbreed: cowbreed,
+	sheep: sheep,
+	sheepbreed: sheepbreed,
+};
+
+export default unitImages;
